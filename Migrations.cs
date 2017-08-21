1c99c54cc9b74afa1e69e563744abb7589a5e3db
Peter Keating <peter@moov2.com>
2017-08-21 16:47:15 +0100

    Add integrations with projections
    Added ability to create a query containing featured content items.
    Also added binding to allow the query results to be ordered by the
    `Weight` value.

--- Migrations.cs
+++ Migrations.cs
@@ -1,11 +1,35 @@
-﻿using Orchard.ContentManagement.MetaData;
+﻿using Moov2.Orchard.Featured.Models;
+using Orchard.ContentManagement.MetaData;
 using Orchard.Core.Contents.Extensions;
+using Orchard.Data;
 using Orchard.Data.Migration;
+using Orchard.Localization;
+using Orchard.Projections.Models;
 
 namespace Moov2.Orchard.Featured
 {
     public class Migrations : DataMigrationImpl
     {
+        #region Dependencies
+
+        private readonly IRepository<MemberBindingRecord> _memberBindingRepository;
+
+        public Localizer T { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Migrations(IRepository<MemberBindingRecord> memberBindingRepository)
+        {
+            _memberBindingRepository = memberBindingRepository;
+            T = NullLocalizer.Instance;
+        }
+
+        #endregion
+
+        #region Implementation
+
         public int Create()
         {
             SchemaBuilder.CreateTable("FeaturedPartRecord",
@@ -18,8 +42,31 @@ namespace Moov2.Orchard.Featured
             ContentDefinitionManager.AlterPartDefinition("FeaturedPart", builder => builder
                 .Attachable()
                 .WithDescription("Flag content as featured."));
-            
-            return 1;
+
+            _memberBindingRepository.Create(new MemberBindingRecord
+            {
+                Type = typeof(FeaturedPartRecord).FullName,
+                Member = "Weight",
+                DisplayName = T("Featured Weight").Text,
+                Description = T("Weight given to featured content item.").Text
+            });
+
+            return 2;
         }
+        
+        public int UpdateFrom1()
+        {
+            _memberBindingRepository.Create(new MemberBindingRecord
+            {
+                Type = typeof(FeaturedPartRecord).FullName,
+                Member = "Weight",
+                DisplayName = T("Featured Weight").Text,
+                Description = T("Weight given to featured content item.").Text
+            });
+
+            return 2;
+        }
+
+        #endregion
     }
 }
---
+++ Projections/FeaturedFilter.cs
@@ -0,0 +1,45 @@
+﻿using Moov2.Orchard.Featured.Models;
+using Orchard.ContentManagement;
+using Orchard.Events;
+using Orchard.Localization;
+using System;
+using System.Linq;
+
+namespace Moov2.Orchard.Featured.Projections
+{
+    public interface IFilterProvider : IEventHandler
+    {
+        void Describe(dynamic describe);
+    }
+
+    public class FeaturedFilter : IFilterProvider
+    {
+        public FeaturedFilter()
+        {
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        public void Describe(dynamic describe)
+        {
+            describe.For("Featured", T("Featured"), T("Featured"))
+                .Element("IsFeatured", T("Is Featured"), T("Featured content items"),
+                    (Action<dynamic>)ApplyFilter,
+                    (Func<dynamic, LocalizedString>)DisplayFilter,
+                    null
+                );
+        }
+
+        public void ApplyFilter(dynamic context)
+        {
+            var query = (IHqlQuery)context.Query;
+            context.Query = query.Where(x => x.ContentPartRecord<FeaturedPartRecord>(), x => x.Eq("IsFeatured", true));
+        }
+
+        public LocalizedString DisplayFilter(dynamic context)
+        {
+            return T("Is Featured");
+        }
+    }
+}
