9b7231d12d4a71976b8ab093b3b2b019b238d4db
Peter Keating <peter@moov2.com>
2017-09-22 12:18:07 +0100

    Only display Weight field when content item is flagged as featured

---
+++ Assets/JavaScript/orchard.featured.admin.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    var $enabledCheckbox = $('.js-featured-enable-field input[type=checkbox]'),
+        $weightField = $('.js-featured-weight-field');
+
+    /**
+     * Updates the visibility of the weight field.
+     */
+    var updateWeightVisibility = function () {
+        if ($enabledCheckbox.is(':checked')) {
+            $weightField.css('display', 'block');
+            return;
+        }
+
+        $weightField.css('display', 'none');
+    };
+
+    updateWeightVisibility();
+    $enabledCheckbox.on('change', updateWeightVisibility);
+})();
---
+++ ResourceManifest.cs
@@ -0,0 +1,13 @@
+﻿using Orchard.UI.Resources;
+
+namespace Moov2.Orchard.Featured
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+            manifest.DefineScript("FeaturedAdminJs").SetUrl("orchard.featured.admin.min.js", "orchard.featured.admin.js").SetDependencies("JQuery");
+        }
+    }
+}
